81ce728f2228a606722d77428a2cc4c3ab28ae85
Sanju2 <lsanjeewa947@gmail.com>
2023-12-29 23:59:51 +0400

    2023-12-29 commit

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "ResumeAPI" {
   filename         = data.archive_file.zip_the_python_code.output_path
   source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
-  function_name    = "ResumeAPI"
+  function_name    = "TestAPI"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.9"
@@ -62,7 +62,7 @@ data "archive_file" "zip_the_python_code" {
 }
 
 resource "aws_lambda_function_url" "url1" {
-  function_name      = aws_lambda_function.ResumeAPI.ResumeAPI
+  function_name      = aws_lambda_function.ResumeAPI.function_name
   authorization_type = "NONE"
 
   cors {
