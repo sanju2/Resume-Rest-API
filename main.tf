4867261a704d516028d8cfe8b02afe049fa9ac4c
Sanju2 <lsanjeewa947@gmail.com>
2023-12-30 10:19:50 +0400

    Test3

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_lambda_function" "ResumeAPI" {
   filename         = data.archive_file.zip_the_python_code.output_path
   source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
-  function_name    = "API24"
+  function_name    = "API89"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.9"
