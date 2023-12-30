a839a5a87ac0424fe3cea49eb21212f620fb2254
Sanju2 <lsanjeewa947@gmail.com>
2023-12-30 09:21:39 +0400

    Test2

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_iam_policy" "iam_policy_for_resume_project" {
   name        = "aws_iam_policy_for_terraform_resume_project_policy"
   path        = "/"
   description = "AWS IAM Policy for managing the resume project role"
-    policy = jsonencode(
+  policy = jsonencode(
     {
       "Version" : "2012-10-17",
       "Statement" : [
@@ -65,9 +65,9 @@ resource "aws_iam_policy" "iam_policy_for_resume_project" {
 }
 
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
-  role = aws_iam_role.iam_for_lambda.name
+  role       = aws_iam_role.iam_for_lambda.name
   policy_arn = aws_iam_policy.iam_policy_for_resume_project.arn
-  
+
 }
 
 data "archive_file" "zip_the_python_code" {
